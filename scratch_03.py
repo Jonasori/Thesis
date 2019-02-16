3239a1b834f7dd9101a92194f809a28afba860c8
Jonas Powell <jonasori@gmail.com>
2019-02-16 09:40:23 -0500

    Updated

--- scratch_03.py
+++ scratch_03.py
@@ -9,6 +9,12 @@ from pathlib2 import Path
 
 from tools import imstat_single
 
+# All MKS
+h = const.h.value
+c = const.c.value
+k = const.k_B.value
+
+
 os.chdir('/Volumes/disks/jonas/modeling')
 Path.cwd()
 
@@ -41,9 +47,11 @@ def miriad_caller(call_str):
 
 
 F = float(miriad_caller(call_str)[0][2])
-#  params = [J_u, B0, T_ex, nu0, F, m, d, A_ul]
-# params_hco = [4, B0, 17, 356.734288, F, 29.0, 389, 3.6269e-3]
-params_hco = [1, 1, 1, 1, 1, 1, 1, 1]
+J = 1
+E0_hco = 2.975 * 1e-2         # cm/s, taken from first non-zero energy in hcoplus.dat
+B0 = E0_hco/(J * (J+1))       # Need this in units of wavenumber?!
+# params = [J_u, B0 (wavenumber), T_ex (K), nu0 (GHz), F (J/beam km/s), m (molecular mass), d (parsecs), A_ul (Hz)]
+params_hco = [4, B0, 17, 356.734288, F, 29.0, 389, 3.6269e-3]
 
 def get_gas_mass(params):
     """
@@ -59,21 +67,20 @@ def get_gas_mass(params):
         d (float): distance to source
         A_ul (float): Einstein coefficient of u-l transition (Hz)
     """
-    # Make sure these units are good
-    h = const.h.value
-    c = const.c.value
-    k = const.k_B.value
 
     J_u, B0, T_ex, nu0, F, m, d, A_ul = params
+    # Straighten out the units (other -> MKS)
     nu0 *= 1e9              # GHz -> Hz
+    B0 *= 1e-2              # cm/s -> m/s in the E0 term.
+    d *= 3.08e16            # parsecs -> m
 
 
     X_u = (2 * J_u + 1) * np.exp(-B0 * J_u * (J_u + 1) * h * c /(k * T_ex))/(k * T_ex/(h * c * B0))
     m_gas = (4 * np.pi/(h * nu0) ) * (F * m * d**2 / (A_ul * X_u))
     return m_gas
 
-get_gas_mass(params_hco)
-
+m = get_gas_mass(params_hco)
+print m
 
 
 
