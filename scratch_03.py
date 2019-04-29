c19e4b56b1b27cf7e97d8f6a93665511ef52d002
Jonas Powell <jonasori@gmail.com>
2019-04-29 19:25:54 -0400

    Updated

--- scratch_03.py
+++ scratch_03.py
@@ -9,10 +9,10 @@ from pathlib2 import Path
 
 
 # All MKS
-h = const.h.value
-c = const.c.value
-k = const.k_B.value
-hbar = const.hbar.value
+# h = const.h.value
+# c = const.c.value
+# k = const.k_B.value
+# hbar = const.hbar.value
 
 
 Path.cwd()
@@ -25,8 +25,8 @@ def get_fluxed():
     from tools import imstat_single
 
     im = 'data/hco/hco-short110_moment0'
-    rms = imstat_single(im)[1]
-    call_str = "cgcurs in={},{} device=/xs type=both slev=a,{} levs=2,7,15 options=stats".format(im + '.cm', im + '.cm', rms)
+    rms = imstat_single(im)[1] # 0.0294
+    call_str = "cgcurs in={},{} device=/xs type=both slev=a,{} levs=3 options=stats region=arcsec,box'(-2,-2,2,2)'".format(im + '.cm', im + '.cm', rms)
     def miriad_caller(call_str):
         call_list = call_str.split(' ')
         l = sp.check_output(call_list)
@@ -56,72 +56,66 @@ k = const.k_B.decompose()
 hbar = const.hbar.decompose()
 
 
-# Einstein A (FCO+(4-3) come from: https://home.strw.leidenuniv.nl/~moldata/datafiles/hco+@xpol.dat
-# Using the value listed as the 5-4 trans since the indexing is bad.
-A_43 =  3.6269e-03 * (1/u.s) # s-1.
-# F = float(miriad_caller(call_str)[0][2])
 
 
-J_hco = 4.
-E_43 = 29.7491 * u.cm.decompose()      #cm-1, from https://home.strw.leidenuniv.nl/~moldata/datafiles/hco+@xpol.dat
-
-E0_hco = 2.975e-2 * (1/u.cm).decompose()    # cm-1, taken from first non-zero energy in hcoplus.dat, converted to m-1
-B0_hco = 2*np.pi*E0_hco
-
-E43_hco = E0_hco/(J_hco * (J_hco+1))
-B_hco43 = 2*np.pi*E43_hco # From wiki: k = E/(hbar c = hc/2pi) -> k (hc) = 2pi E
-
-d = 389 * u.pc.decompose()  # pc -> m
-nu0 = 356.734288 * u.GHz.decompose()
-mol_mass_hco = 29.0 # Molecular weight - should this have units?
-T_ex_hco = 17 * u.K # From Sam
-
-F_hco_jy = 4.15 * u.Jy * u.km * u.s**(-1)  #Jy km/s
-F_hco = (F_hco_jy * nu0/c * 1e5).decompose()
-
-F_hco.decompose()
-
-
-# params = [J_u, B0 (wavenumber), T_ex (K), nu0 (GHz), F (J/beam km/s), m (molecular mass), d (parsecs), A_ul (Hz)]
-params = [J_hco, B0_hco.value, T_ex_hco.value, nu0.value, F_hco.value, mol_mass_hco, d.value, A_43.value]
-params = [J_hco, B0_hco, T_ex_hco, nu0, F_hco, mol_mass_hco, d, A_43]
-
-def get_gas_mass(params):
+def get_mass():
     """
-    Calculate a value for Eqn 3.1 in Factor et al 2017.
+    Calculate gas mass of a disk from its flux in cgs.
 
-    Args (unpacked):
-        J_u (int): Upper level's quantum number.
-        B0 (int): Rotational constant (units of wavenumber)
-        T_ex (float): Excitational temperature (K)
-        nu0 (float): restfreq (GHz)
-        F (float): integrated line flux (get from cgcurs)
-        m (float): mass of emitting molecule (units?)
-        d (float): distance to source
-        A_ul (float): Einstein coefficient of u-l transition (Hz)
+    Sources:
+        - F: Convert cgcurs output (6.x) from Jy km/s by multiplying by
+                [1e-23 erg/(cm2 s Hz) Hz][nu0/(c km/s) * 1e5 (cm s-1/km s-1)]
+        - A_ul: Einstein A, HCO+(4-3) (listed as 5-4 trans bc indexing is bad)
+                https://home.strw.leidenuniv.nl/~moldata/datafiles/hco+@xpol.dat
+        - T: Excitation temperature from Factor et al (2017)
+        - B0: From wiki: B = k = E/(hbar c = hc/2pi) -> k (hc) = 2pi E
+            - Unclear whether this should use E = (E0 = 0.0297) or E = (E_43 = 29.75) cm-1
+                - Both from hcoplus.dat and
+                https://home.strw.leidenuniv.nl/~moldata/datafiles/hco+@xpol.dat
+        - m: molecular weight (g/mol), divided by Avagadro -> g
+            - weight: 29, from https://home.strw.leidenuniv.nl/~moldata/datafiles/hco+@xpol.dat
+
+
+    Questions:
+        - B0: Using E0 vs. E43. Mostly concerned about this.
+        - Seems like Xu should be unitless, but
+            a) it's not
+            b) it cancels with the leftovers from the rest of the eq (Hz/Hz)
     """
-    J_u, B0, T_ex, nu0, F, m, d, A_ul = params
-    # Straighten out the units (other -> MKS)
+
+    # Disk-specifics
+    F = 6.8 * 1e-12 # erg cm-2 s-1 Hz-1 Hz
+    Aul = 3.63 * 1e-3 # Hz
+    B0 = 2 * np.pi * 0.0297 #29.75
+    T = 17 # K
+    d = 389 * 3.086e+18 # cm
+
+    # Line-specifics
+    m = 29 / (6 * 1e23) # g
+    nu0 = 356.73 * 1e9 # Hz
+    J = 4
+
+    # Constants
+    h = 6.626 * 1e-27 # erg s
+    c = 3e10 # cm/s
+    k = 1.38 * 1e-16 # erg/K
+    g2mearth = 5.92 * 1e27 # g/Mearth
 
 
-    # This is cheating - the units aren't dead yet.
-    Xu_exp = ((-B0 * J_u * (J_u + 1) * h * c) /(k * T_ex))
-    Xu_coeff = ((2 * J_u + 1) / (k * T_ex/(h * c * B0)))
 
-    Xu_exp
 
-    Xu_coeff
+    Xu_exp = ((-B0 * J * (J + 1) * h * c) /(k * T))
+    Xu_coeff = ((2 * J + 1) / (k * T/(h * c * B0)))
 
     Xu = Xu_coeff * np.exp(Xu_exp)
 
-    m_gas = (4 * np.pi/(h.decompose().value * nu0.decompose().value) ) * (F.decompose().value * m * d.decompose().value**2 / (A_ul.decompose().value * Xu))
-    m_gas /= 1.9891e30 # Put it in solar masses.
-    return m_gas
+    m_gas = (4 * np.pi/(h * nu0) ) * (F * m * d**2 / (Aul * Xu))
+    # m_gas /= 1e-8   # Scale by X_hco to get total mass
+    m_gas_mearth = m_gas/g2mearth
+    return m_gas_mearth
 
-m = get_gas_mass(params)
-print m
 
-m_gas
+get_mass()
 
 
 
@@ -140,6 +134,39 @@ def get_stellar_mass():
 
 
 
+
+
+
+
+
+
+
+nu0 = 356.73 * 1e9 # Hz
+F = 7.13 * 1e-12 # erg cm-2 s-1 Hz-1 Hz
+d = 3e18 # cm
+m = 29 / (6 * 1e23) # g
+h = 6.626 * 1e-27 # erg s
+Aul = 3.63 * 1e-3 # Hz
+J = 4
+T = 17 # K
+B0 = 2 * np.pi * 0.0297
+c = 3e10 # cm/s
+k = 1.38 * 1e-16 # erg/K
+
+g2mearth = 5.92 * 1e27 # g/Mearth
+
+def new_get_mass():
+    Xu_exp = ((-B0 * J * (J + 1) * h * c) /(k * T))
+    Xu_coeff = ((2 * J + 1) / (k * T/(h * c * B0)))
+
+    Xu = Xu_coeff * np.exp(Xu_exp)
+
+    m_gas = (4 * np.pi/(h * nu0) ) * (F * m * d**2 / (Aul * Xu))
+    m_gas_mearth = m_gas/g2mearth
+    return m_gas_mearth
+
+
+new_get_mass()
 
 
 
